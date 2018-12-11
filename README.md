ebd7ebb36bd93c553c90ebcbe88e2150a3b53243
Alexander F. Rødseth <rodseth@gmail.com>
2018-12-11 14:16:02 +0100

    Update command to be able to take an argument

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # seasons
 
-Change desktop wallpaper throughout the day, for any windowmanager
+Change desktop wallpaper throughout the day, for any windowmanager.
+
+Will also set the wallpaper if an image filename is given as the first argument.
 
 # Work in progress
 
-Currently changes the wallpaper to a random image from `/usr/share/pixmaps`!
+Currently changes the wallpaper to a random image from `/usr/share/pixmaps` if no argument is given!
 
 The plan is to first support a large collection of windowmanagers and ways of setting the wallpaper.
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"path/filepath"
 	"time"
 
@@ -11,12 +12,24 @@ import (
 
 func main() {
 	imageFilename := ""
-	if matches, err := filepath.Glob("/usr/share/pixmaps/*.png"); err != nil {
-		panic(err)
+	if len(os.Args) > 1 {
+		imageFilename = os.Args[1]
 	} else {
-		rand.Seed(time.Now().Unix())
-		randomIndex := rand.Int() % len(matches)
-		imageFilename = matches[randomIndex]
+		if matches, err := filepath.Glob("/usr/share/pixmaps/*.png"); err != nil {
+			panic(err)
+		} else {
+			rand.Seed(time.Now().Unix())
+			randomIndex := rand.Int() % len(matches)
+			imageFilename = matches[randomIndex]
+		}
+	}
+	absImageFilename, err := filepath.Abs(imageFilename)
+	if err == nil {
+		imageFilename = absImageFilename
+	}
+	if _, err := os.Stat(imageFilename); os.IsNotExist(err) {
+		// File does not exist
+		panic(err)
 	}
 	fmt.Println("Setting background image to: " + imageFilename)
 	if err := wallpaper.Set(imageFilename); err != nil {
